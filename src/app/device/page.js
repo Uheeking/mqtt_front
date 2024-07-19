71734bd4f4fbb32e8fd6f4dd4bc444a997199be7
Uheeking <a@infob.co.kr>
2024-07-19 14:02:14 +0900

    ⚡️ MODIFY : 이전 페이지로 이동

--- src/app/device/page.js
+++ src/app/device/page.js
@@ -3,9 +3,12 @@
 import React, { useState } from 'react';
 import DeviceForm from '@/components/component/DeviceForm';
 import DeviceList from '@/components/component/DeviceList';
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
+import { useRouter } from 'next/navigation';
 
 export default function DevicePage() {
   const [devices, setDevices] = useState([]);
+  const router = useRouter();
 
   const handleAddDevice = (device) => {
     setDevices([...devices, device]);
@@ -19,7 +22,11 @@ export default function DevicePage() {
 
   return (
     <div className="flex flex-col items-center w-full min-h-screen p-4">
-      <header className="w-full py-4 text-center border-b">
+      <header className="w-full py-4 border-b flex items-center justify-center relative">
+        <MdKeyboardDoubleArrowLeft 
+          className="absolute left-4 cursor-pointer text-2xl mb-3" 
+          onClick={() => router.back()} 
+        />
         <h1 className="text-xl font-bold">디바이스 등록 및 목록</h1>
       </header>
       <main className="flex flex-col items-center w-full mt-4">
