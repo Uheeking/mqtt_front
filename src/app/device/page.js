4e442fd19f89af9c39881b4e2492cbb2fcf53939
Uheeking <a@infob.co.kr>
2024-07-19 16:42:52 +0900

    ⚡️ MODIFY : page path changed

--- src/app/device/page.js
+++ src/app/device/page.js
@@ -1,6 +1,7 @@
 // src/app/device/page.js
 'use client'
 import React, { useState } from 'react';
+import Link from 'next/link';
 import DeviceForm from '@/components/component/DeviceForm';
 import DeviceList from '@/components/component/DeviceList';
 import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
@@ -23,10 +24,11 @@ export default function DevicePage() {
   return (
     <div className="flex flex-col items-center w-full min-h-screen p-4">
       <header className="w-full py-4 border-b flex items-center justify-center relative">
-        <MdKeyboardDoubleArrowLeft 
-          className="absolute left-4 cursor-pointer text-2xl mb-3" 
-          onClick={() => router.back()} 
-        />
+        <Link href="/" legacyBehavior>
+          <MdKeyboardDoubleArrowLeft
+            className="absolute left-4 cursor-pointer text-2xl mb-"
+          />
+        </Link>
         <h1 className="text-xl font-bold">디바이스 등록 및 목록</h1>
       </header>
       <main className="flex flex-col items-center w-full mt-4">
